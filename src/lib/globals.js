7a125a544ef03aed0c7325ac597eac0499eddd9f
Alex Monteverde <alexmo@avi.com>
2023-06-09 17:44:47 -0400

    Updating APPURL constant for prod

--- src/lib/globals.js
+++ src/lib/globals.js
@@ -1,5 +1,5 @@
 export const APPURL = process.env.NODE_ENV === "development" ?
-"http://localhost:3000" : "http://localhost:3000";
+"http://localhost:3000" : "https://wedding-mailing-list.vercel.app";
 
 export const PRIMARY_COLOR = "#2e4082";
 export const PRIMARY_WHITE = "#FAFAFA";
